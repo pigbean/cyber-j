7d142a4a8aa013caf6af16778dcd268b7c087937
pigbean <skdie3489@gmail.com>
2017-06-07 14:20:57 +0900

    ArithmeticException 처리

--- Java/src/scu/jhg.java
+++ Java/src/scu/jhg.java
@@ -1,15 +1,24 @@
 package scu;
 
+import java.util.Scanner; 
+
 public class jhg
 {
-	static int sum;
-	static int minu;
-	static int multi;
-	static int div;
+	static int sum=0;
+	static int minu=0;
+	static int multi=0;
+	static double div=0;
 
 	public static void main(String[] args)
 	{
-		output(50,50);
+		Scanner sc = new Scanner(System.in);		
+		
+		System.out.print("첫번째 숫자 입력 : ");
+		int num1 = sc.nextInt();
+		System.out.print("두번째 숫자 입력 : ");
+		int num2 = sc.nextInt();		
+				
+		output(num1, num2);
 		System.out.print("num1 + num2 : "+sum);
 		System.out.print("\nnum1 - num2 : "+minu);
 		System.out.print("\nnum1 * num2 : "+multi);
@@ -18,17 +27,20 @@ public class jhg
 
 	private static void output(int i, int j)
 	{
-		if(i == 0 || j == 0)
-		{
-			return;
-		}
-		else
+		try
 		{
 			sum = i + j;
 			minu = i - j;
 			multi = i * j;
 			div = i / j;
 		}
+		catch(ArithmeticException ae)
+		{
+			System.out.print("0으로 나눌 수 없습니다.");
+		}
+	
+	
 	}
+	
 	
 }
