e7b1e5894842b3adb58ebc51dc7ad148e64a566e
pigbean <skdie3489@gmail.com>
2017-06-07 15:39:07 +0900

    clean

--- Java/src/scu/jhg.java
+++ Java/src/scu/jhg.java
@@ -4,28 +4,49 @@ import java.util.Scanner;
 
 public class jhg
 {
-	static int sum=0;
-	static int minu=0;
-	static int multi=0;
-	static double div=0;
+	
+	public static int sum=0;
+	public static int minu=0;
+	public static int multi=0;
+	public static double div=0;
+	
+	private static Scanner sc;
 
 	public static void main(String[] args)
 	{
-		Scanner sc = new Scanner(System.in);		
+		sc = new Scanner(System.in);		
 		
 		System.out.print("첫번째 숫자 입력 : ");
 		int num1 = sc.nextInt();
 		System.out.print("두번째 숫자 입력 : ");
 		int num2 = sc.nextInt();		
 				
-		output(num1, num2);
+		scu.cals.output(num1, num2);
+		
 		System.out.print("num1 + num2 : "+sum);
 		System.out.print("\nnum1 - num2 : "+minu);
 		System.out.print("\nnum1 * num2 : "+multi);
 		System.out.print("\nnum1 / num2 : "+div);
+		
 	}
+	/*
+	System.out.print("num1 + num2 : "+sum);
+	System.out.print("\nnum1 - num2 : "+minu);
+	System.out.print("\nnum1 * num2 : "+multi);
+	System.out.print("\nnum1 / num2 : "+div);
+	*/
+}
 
-	private static void output(int i, int j)
+public class cals extends jhg
+{
+	/*
+	static int sum;
+	static int minu;
+	static int multi;
+	static double div;
+	*/
+	
+	public static void output(int i, int j)
 	{
 		try
 		{
@@ -38,9 +59,14 @@ public class jhg
 		{
 			System.out.print("0으로 나눌 수 없습니다.");
 		}
-	
-	
+		/*
+		System.out.print("num1 + num2 : "+sum);
+		System.out.print("\nnum1 - num2 : "+minu);
+		System.out.print("\nnum1 * num2 : "+multi);
+		System.out.print("\nnum1 / num2 : "+div);
+		*/
 	}
 	
-	
 }
+
+	
