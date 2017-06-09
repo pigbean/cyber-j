ab3e8b2a97a0e075ebcee124d88df747f147437a
pigbean <skdie3489@gmail.com>
2017-06-09 11:36:56 +0900

    Class 이름 변경 및 요구사항 제거 객체생성

--- Java/src/scu/Jhg.java
+++ Java/src/scu/Jhg.java
@@ -1,39 +1,9 @@
 package scu;
-/* 요구사항
-1. 클래스 이름은 본인 이름 이니셜
-2. 사칙연산 클래스 : 두 수를 인수로 받아 사칙연산 결과를 출력(+ - * / 메소드)
-3. 나눗셈 메소드에서 0으로 나눌때 예외처리
-4. 실행 클래스 : 사칙연산 클래스를 상속(두개의 인수를 입력받아 사칙연산 메소드 호출을 통해 결과 출력)
-
-
-
-1. 사칙연산을 수행하는 클래스를 생성하고 해당 클래스를 상속하여 사칙연산을 수행하는 프로그램 개발
-	1-1. 클래스 이름은 본인의 이니셜 사용 -> JHG
-	1-2. 사칙연산 클래스 : 두 수를 인수로 받아 사칙연산 결과를 출력
-		덧셈, 뺄셈, 곱셈, 나눗셈
-		나눗셈 메소드의 경우 0으로 나누는 경우의 예외처리
-	1-3. 실행 클래스 : 사칙연산 클래스를 상속
-		실행 시 두개의 인수를 입력받아 사칙연산 메소드 호출을 통해 결과 출력
-		학번/이름/일시를 화면에 출력
-
-실행 예
--java jhg 4 2
-
-실행 결과
-실행자 : 16054435 지호근
-실행일 : 오늘 날짜는 
-
-실행결과
-1. 4 + 2 = 6
-2. 4 - 2 = 2
-3. 4 * 2 = 8
-4. 4 / 2 = 2
- */
 
 import java.util.Scanner; 
 import java.util.Date;
 
-class jhg extends cals
+class Jhg extends Cals
 {
 
 	private static Scanner sc;
@@ -46,8 +16,9 @@ class jhg extends cals
 		int num1 = sc.nextInt();
 		System.out.print("두번째 숫자 입력 : ");
 		int num2 = sc.nextInt();		
-				
-		scu.cals.output(num1, num2);
+		
+		Jhg jhg = new Jhg();
+		jhg.output(num1, num2);
 		
 		System.out.printf("실행자 : 11604435 지호근\n");
 		System.out.printf("오늘 날짜는  %tY년 %tm월 %td일\n", new Date(), new Date(), new Date());
@@ -62,7 +33,7 @@ class jhg extends cals
 
 }
 
-class cals
+class Cals
 {
 	
 	static int sum;
