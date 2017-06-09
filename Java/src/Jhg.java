d090ac2b95fe0c05f4cc6c17ecf7ee9fa0efedb4
pigbean <skdie3489@gmail.com>
2017-06-09 15:54:27 +0900

    실행자와 날짜는 항상 노출로 순서 변경

--- Java/src/Jhg.java
+++ Java/src/Jhg.java
@@ -13,12 +13,13 @@ class Jhg extends Cals
 		int num1 = sc.nextInt();
 		int num2 = sc.nextInt();		
 		
-		Jhg jhg = new Jhg(); 
-		jhg.output(num1, num2); // 사칙연산 실행 Class 호출
-		
+	
 		System.out.printf("실행자 : 11604435 지호근\n");
 		System.out.printf("오늘 날짜는  %tY년 %tm월 %td일\n", new Date(), new Date(), new Date());
-				
+		
+		Jhg jhg = new Jhg(); 
+		jhg.output(num1, num2); // 사칙연산 실행 Class 호출
+							
 		System.out.printf("\n실행결과\n");
 		System.out.printf("1. %d + %d : %d\n", num1, num2, sum);
 		System.out.printf("2. %d - %d : %d\n", num1, num2, minu);
@@ -49,7 +50,7 @@ class Cals //사칙연산 실행 Class
 		}
 		catch(ArithmeticException ae) // 예외처리 
 		{
-			System.out.print("0으로 나눌 수 없습니다.");
+			System.out.print("0으로 나눌 수 없습니다. 다시 실행해주세요");
 			System.exit(0); // 0으로 나누기 실행 시 계산없이 종료
 		}
 	
